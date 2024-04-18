50250702db8de2c29fea43f133d231399945ca2d
strandedorca <huyendang1497@gmail.com>
2024-04-18 23:51:19 +0700

    Setting up & importing FontAwesome

---
+++ src/components/FAQs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FAQs() {
+  return <div></div>;
+}
+
+export default FAQs;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Menu() {
+  return (
+    <div>
+      <div className="logo"></div>
+      <FontAwesomeIcon icon="bars" />
+    </div>
+  );
+}
+
+export default Menu;
